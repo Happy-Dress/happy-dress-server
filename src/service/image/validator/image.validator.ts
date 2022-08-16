ed1a24ab51d4b850e48e21bbb4d314b89e36d36e
eviv2206 <zhenya.saprin@mail.ru>
2022-08-17 01:46:51 +0300

    fix(HAP-63): improve validator

--- src/service/image/validator/image.validator.ts
+++ src/service/image/validator/image.validator.ts
@@ -1,17 +1,38 @@
-import { IMAGE_EXTENSIONS, MAX_IMAGE_SIZE, MIN_IMAGE_SIZE } from '../constants/validate-image/validate-image-constants';
+import { IMAGE_EXTENSIONS, MAX_IMAGE_SIZE, MIN_IMAGE_SIZE } from '../constants/validate-image/validate-image.constants';
 import { Injectable } from '@nestjs/common';
-import { InvalidImageError } from '../excpetion/invalid-image.error';
+import { FailedUploadResult } from '../model/FailedUploadResult';
+import { INVALID_EXTENSION, INVALID_SIZE } from '../../../../exceptions/constants/image.exception.constants';
+import { Image } from '../model/Image';
 
 @Injectable()
 export class ImageValidator {
-    public validate(images: Express.Multer.File[]): void {
-        images.every((image) => {
-            if (!IMAGE_EXTENSIONS.includes(image.mimetype)){
-                throw new InvalidImageError('Invalid extension.', image.originalname);
+    public getInvalidImages(images: Image[]): FailedUploadResult[] {
+        const failedImages: FailedUploadResult[] = [];
+        images.forEach(image => {
+            if (!this.isValidExtension(image)) {
+                failedImages.push({
+                    id: image.id,
+                    imageName: image.originalname,
+                    reason: `${INVALID_EXTENSION} ${image.mimetype}` });
+                return;
             }
-            if (!(image.size > MIN_IMAGE_SIZE && image.size < MAX_IMAGE_SIZE)) {
-                throw new InvalidImageError('Invalid size.', image.originalname);
+            if (!this.isValidSize(image)) {
+                failedImages.push({
+                    id: image.id,
+                    imageName: image.originalname,
+                    reason: `${INVALID_SIZE}` });
+                return;
             }
         });
+        return failedImages;
     }
+    
+    private isValidExtension(image: Express.Multer.File): boolean {
+        return IMAGE_EXTENSIONS.includes(image.mimetype);
+    }
+    
+    private isValidSize(image: Express.Multer.File): boolean {
+        return image.size > MIN_IMAGE_SIZE && image.size < MAX_IMAGE_SIZE;
+    }
+    
 }
