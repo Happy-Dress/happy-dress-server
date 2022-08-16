d17c931bff0c0bd2d82bb4fc95511d8916aa504b
eviv2206 <zhenya.saprin@mail.ru>
2022-08-17 01:49:56 +0300

    feat(HAP-63): add exception constants

---
+++ exceptions/constants/image.exception.constants.ts
@@ -0,0 +1,3 @@
+export const INVALID_EXTENSION = 'Неверное расширение файла:';
+export const INVALID_SIZE = 'Неверный размер файла';
+export const FAILED_UPLOAD_GOOGLE_DRIVE = 'Ошибка загрузки фото на google-drive';
