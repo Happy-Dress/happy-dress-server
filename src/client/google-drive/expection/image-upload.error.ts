dd80ce8c8b381fb4e68ae721fe9dd8568b399f49
eviv2206 <zhenya.saprin@mail.ru>
2022-08-17 01:50:51 +0300

    feat(HAP-63): add image upload error

--- src/client/google-drive/expection/image-upload.error.ts
+++ src/client/google-drive/expection/image-upload.error.ts
@@ -1,5 +1,14 @@
+import { FailedUploadResult } from '../../../service/image/model/FailedUploadResult';
+
 export class ImageUploadError extends Error {
-    constructor(imageName: string) {
-        super(`Error uploading image ${imageName}`);
+    private readonly failedImage: FailedUploadResult;
+
+    constructor(failedImage: FailedUploadResult) {
+        super(failedImage.reason);
+        this.failedImage = failedImage;
+    }
+
+    public getFailedImage(): FailedUploadResult {
+        return this.failedImage;
     }
 }
--- src/service/image/excpetion/invalid-image.error.ts
+++
@@ -1,5 +0,0 @@
-export class InvalidImageError extends Error {
-    constructor(message: string, imageName: string) {
-        super(`${message} image: ${imageName}`);
-    }
-}
