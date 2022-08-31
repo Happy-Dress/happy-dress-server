5cef75f9defd17874422dc3d9521b749db662571
eviv2206 <zhenya.saprin@mail.ru>
2022-09-01 01:22:29 +0300

    fix(HAP-87): change testing with it each

--- src/app/service/image/validator/image.validation.pipe.ts
+++ src/app/service/image/validator/image.validation.pipe.ts
@@ -4,10 +4,11 @@ import {
     BadRequestException,
 } from '@nestjs/common';
 import { NULL_IMAGES_DETECTED } from '../../../messages/constants/messages.constants';
+import { Image } from '../model/Image';
 
 @Injectable()
 export class NullFileValidationPipe implements PipeTransform {
-    public transform( files: Express.Multer.File[] ): Express.Multer.File[] {
+    public transform( files: Image[] ): Image[] {
         if (!files?.length || !Array.isArray(files)) {
             throw new BadRequestException(NULL_IMAGES_DETECTED);
         }
--- src/test/service/image/validator/image.validation.pipe.spec.ts
+++ src/test/service/image/validator/image.validation.pipe.spec.ts
@@ -6,35 +6,28 @@ import { NULL_IMAGES_DETECTED } from '../../../../app/messages/constants/message
 
 describe('NullFileValidationPipe', () => {
     let nullFileValidationPipe: NullFileValidationPipe;
-
+    const error = new BadRequestException(NULL_IMAGES_DETECTED);
+    
     beforeEach( () => {
         nullFileValidationPipe = new NullFileValidationPipe();
     });
 
     describe('upload',  () => {
         it('should validate files and return them', () => {
-            const files: Image[] = [
+            const files = [
                 {
                     id: 0,
                 },
-            ] as any[];
+            ] as Image[];
             const result = files;
             const actualResult = nullFileValidationPipe.transform(files);
             expect(actualResult).toBe(result);
         });
-        it('should throw error 400, reason: array is empty', () => {
-            const files = [] as any[];
-            const error = new BadRequestException(NULL_IMAGES_DETECTED);
-            expect(() => {
-                nullFileValidationPipe.transform(files);
-            }).toThrow(error);
-        });
-        it('should throw error 400, reason: file is null ', () => {
-            const files = null as Express.Multer.File;
-            const error = new BadRequestException(NULL_IMAGES_DETECTED);
-            expect(() => {
-                nullFileValidationPipe.transform(files);
-            }).toThrow(error);
+        it.each([
+            { files: [] as Image[] },
+            { files: null as Image[] }, 
+        ])('should throw an exception 400', ({ files }) => {
+            expect(() => nullFileValidationPipe.transform(files)).toThrow(error);
         });
     });
 });
