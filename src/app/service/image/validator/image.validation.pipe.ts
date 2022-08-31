cfe206d2a2a304eedc7d80736b74b10a4b610d9a
eviv2206 <zhenya.saprin@mail.ru>
2022-08-31 21:39:47 +0300

    fix(HAP-87): change validation condition

--- src/app/service/image/validator/image.validation.pipe.ts
+++ src/app/service/image/validator/image.validation.pipe.ts
@@ -7,13 +7,8 @@ import { NULL_IMAGES_DETECTED } from '../../../messages/constants/messages.const
 
 @Injectable()
 export class NullFileValidationPipe implements PipeTransform {
-    public transform(
-        files: Express.Multer.File | Express.Multer.File[]
-    ): Express.Multer.File | Express.Multer.File[] {
-        if (files === undefined || files === null) {
-            throw new BadRequestException(NULL_IMAGES_DETECTED);
-        }
-        if (Array.isArray(files) && files.length === 0) {
+    public transform( files: Express.Multer.File[] ): Express.Multer.File[] {
+        if (!files?.length || !Array.isArray(files)) {
             throw new BadRequestException(NULL_IMAGES_DETECTED);
         }
         return files;
