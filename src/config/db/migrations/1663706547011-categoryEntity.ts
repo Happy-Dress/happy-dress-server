c1ec30ea269f1bd6922ec846b45ef0c24d3437df
eviv2206 <zhenya.saprin@mail.ru>
2022-09-28 14:45:45 +0300

    feat(HAP-95): add migration category

---
+++ src/config/db/migrations/1663706547011-categoryEntity.ts
@@ -0,0 +1,34 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class categories1663706547011 implements MigrationInterface {
+
+
+  public async up(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.createTable(
+            new Table({
+              name: 'category',
+              columns: [
+                {
+                  name: 'id',
+                  type: 'int4',
+                  isPrimary: true,
+                  isGenerated: true,
+                  generationStrategy: 'increment',
+                },
+                {
+                  name: 'description',
+                  type: 'varchar',
+                  isUnique: true,
+                  isNullable: false,
+                },
+              ],
+            }),
+            false,
+        );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('DROP TABLE category');
+  }
+
+}
