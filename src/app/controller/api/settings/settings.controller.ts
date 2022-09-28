302bdd0d0b2b89d8affb445685098fedcb36cf72
eviv2206 <zhenya.saprin@mail.ru>
2022-09-28 14:54:45 +0300

    feat(HAP-95): create settings controller

---
+++ src/app/controller/api/settings/settings.controller.ts
@@ -0,0 +1,23 @@
+import { Body, Controller, Get, Inject, Post, UseGuards } from '@nestjs/common';
+import { GlobalDressOptionsDTO } from '../../../service/settings/model/GlobalDressOptionsDTO';
+import { JwtAuthGuard } from '../../security/guards/jwt.auth.guard';
+import { ISettingsService } from '../../../service/settings/settings.service.abstraction';
+
+@Controller('settings')
+export class SettingsController {
+
+    @Inject()
+    private settingsService: ISettingsService;
+
+    @Get()
+    async get(): Promise<GlobalDressOptionsDTO> {
+      return this.settingsService.getGlobalDressOptions();
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Post()
+    async save(@Body() globalDressOptionsDTO: GlobalDressOptionsDTO): Promise<GlobalDressOptionsDTO> {
+      return this.settingsService.setGlobalDressOptions(globalDressOptionsDTO);
+    }
+
+}
