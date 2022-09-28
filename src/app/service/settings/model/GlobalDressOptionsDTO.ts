d94d9422a9404b725b8aa5fd0e44c03f4adc5939
eviv2206 <zhenya.saprin@mail.ru>
2022-09-28 14:53:10 +0300

    feat(HAP-95): add global dress options DTO(work in progress...)

---
+++ src/app/service/settings/model/GlobalDressOptionsDTO.ts
@@ -0,0 +1,17 @@
+import { CategoryDTO } from './CategoryDTO';
+// import {ModelDTO} from "./ModelDTO";
+// import {ColorDTO} from "./ColorDTO";
+// import {MaterialDTO} from "./MaterialDTO";
+
+export class GlobalDressOptionsDTO {
+    categories: CategoryDTO[];
+    // models: ModelDTO[];
+    // materials: MaterialDTO[];
+    // colors: ColorDTO[];
+    
+
+
+
+
+
+}
