59fa78ac769e6cbfa94a1ab67fdc68199bf302a1
eviv2206 <zhenya.saprin@mail.ru>
2022-09-29 14:21:00 +0300

    fix(HAP-95): improve message fail

--- src/app/messages/constants/messages.constants.ts
+++ src/app/messages/constants/messages.constants.ts
@@ -1,21 +1,13 @@
 import {
-  COEFFICIENT_BYTES_TO_MEGABYTES,
-  IMAGE_EXTENSIONS,
   MAX_AMOUNT,
-  MAX_IMAGE_SIZE,
-  MIN_IMAGE_SIZE,
 } from '../../service/image/constants/validate-image/validate-image.constants';
 
 // Image upload messages
-export const INVALID_EXTENSION_MESSAGE = 'Неверное расширение файла:';
-export const VALID_EXTENSION_MESSAGE = `Доступные расширения: ${IMAGE_EXTENSIONS}`;
-export const INVALID_SIZE_MESSAGE = 'Неверный размер файла';
-export const VALID_SIZE_MESSAGE = `Файл должен быть более ${MIN_IMAGE_SIZE * COEFFICIENT_BYTES_TO_MEGABYTES} Мб и не более ${MAX_IMAGE_SIZE * COEFFICIENT_BYTES_TO_MEGABYTES} Мб`;
-export const CURRENT_SIZE_MESSAGE = 'Текущий размер файла:';
+export const INVALID_EXTENSION_MESSAGE = 'Неверное расширение файла: $current. Доступные расширения: $valid.';
+export const INVALID_SIZE_MESSAGE = 'Файл должен быть более $min Мб и не более $max Мб. Текущий размер файла: $current Мб.';
 export const FAILED_UPLOAD_GOOGLE_DRIVE_MESSAGE = 'Ошибка загрузки фото на google-drive';
 export const INVALID_IMAGES_AMOUNT_MESSAGE = `Можно загрузить не более ${MAX_AMOUNT} изображений`;
 export const NULL_IMAGES_DETECTED = 'При отправке запроса на сервер файлы не могут быть пустым значением';
-
 
 // Authentication messages
 export const INVALID_LOGIN_OR_PASSWORD = 'Неверный логин или пароль';
