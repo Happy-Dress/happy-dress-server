4bcc776ede3c18a45598a626f60b607facce3cef
eviv2206 <zhenya.saprin@mail.ru>
2022-10-03 21:28:12 +0300

    feat(HAP-93): init model entity

---
+++ src/app/repository/category/entity/model.entity.ts
@@ -0,0 +1,6 @@
+import { Entity } from 'typeorm';
+import { SimpleListSettingEntity } from './simpleListSetting.entity';
+
+@Entity({ name: 'model' })
+export class ModelEntity extends SimpleListSettingEntity{
+}
