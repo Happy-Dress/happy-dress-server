0a95fc9d113f34bac90ab7b30b34c6ef2fdd3520
eviv2206 <zhenya.saprin@mail.ru>
2022-10-08 14:53:37 +0300

    feat(HAP-93): revert

--- src/app/service/image/validator/image.validator.ts
+++ src/app/service/image/validator/image.validator.ts
@@ -7,10 +7,9 @@ import {
 import { Injectable } from '@nestjs/common';
 import { FailedUploadResult } from '../model/FailedUploadResult';
 import {
-  CURRENT_SIZE_MESSAGE,
   INVALID_EXTENSION_MESSAGE,
   INVALID_IMAGES_AMOUNT_MESSAGE,
-  INVALID_SIZE_MESSAGE, VALID_EXTENSION_MESSAGE, VALID_SIZE_MESSAGE,
+  INVALID_SIZE_MESSAGE,
 } from '../../../messages/constants/messages.constants';
 import { Image } from '../model/Image';
 import { ImageValidationResult } from '../model/ImageValidationResult';
@@ -63,14 +62,29 @@ export class ImageValidator {
       reason,
     };
   }
-    
+
   private checkIsValidExtension(image: Express.Multer.File): string {
-    return IMAGE_EXTENSIONS.includes(image.mimetype) ? '' :
-      `${INVALID_EXTENSION_MESSAGE} ${image.mimetype} ${VALID_EXTENSION_MESSAGE}`;
+    return IMAGE_EXTENSIONS.includes(image.mimetype) ? '' : this.getInvalidExtensionMessage(image.mimetype);
   }
-    
+
   private checkIsValidSize(image: Express.Multer.File): string {
-    return image.size > MIN_IMAGE_SIZE && image.size < MAX_IMAGE_SIZE ? '' :
-      `${INVALID_SIZE_MESSAGE} ${VALID_SIZE_MESSAGE}. ${CURRENT_SIZE_MESSAGE} ${image.size * COEFFICIENT_BYTES_TO_MEGABYTES} Мб.` ;
+    return image.size > MIN_IMAGE_SIZE && image.size < MAX_IMAGE_SIZE ? '' : this.getInvalidSizeMessage(image.size);
   }
+
+  private getInvalidSizeMessage(imageSize: number): string{
+    const minSizeMB = (MIN_IMAGE_SIZE * COEFFICIENT_BYTES_TO_MEGABYTES).toFixed(2);
+    const maxSizeMB = (MAX_IMAGE_SIZE * COEFFICIENT_BYTES_TO_MEGABYTES).toFixed(2);
+    const currSizeMB = (imageSize * COEFFICIENT_BYTES_TO_MEGABYTES).toFixed(2);
+    return INVALID_SIZE_MESSAGE
+      .replace('$min', minSizeMB)
+      .replace('$max', maxSizeMB)
+      .replace('$current', currSizeMB);
+  }
+
+  private getInvalidExtensionMessage(imageType: string): string{
+    return INVALID_EXTENSION_MESSAGE
+      .replace('$current', imageType)
+      .replace('$valid', IMAGE_EXTENSIONS.toString());
+  }
+
 }
