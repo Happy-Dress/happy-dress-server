260f93f08d6bf352323c50da454dbe52b1e7df24
eviv2206 <zhenya.saprin@mail.ru>
2022-10-13 01:14:34 +0300

    feat(HAP-93): add new logic of adding model to database

--- src/app/service/settings/impl/settings.service.ts
+++ src/app/service/settings/impl/settings.service.ts
@@ -1,4 +1,4 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { BadRequestException,  Inject, Injectable } from '@nestjs/common';
 import { ISettingsService } from '../settings.service.abstraction';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
@@ -9,6 +9,8 @@ import { ModelEntity } from '../../../repository/settings/model/entity/model.ent
 import { Transactional } from 'typeorm-transactional';
 import { CategoryConverter } from '../util/category.converter';
 import { CategoryDTO } from '../model/CategoryDTO';
+import { SimpleListSetting } from '../model/SimpleListSetting';
+import { INVALID_ID_TO_UPDATE } from '../../../messages/constants/messages.constants';
 
 @Injectable()
 export class SettingsService implements ISettingsService {
@@ -38,19 +40,41 @@ export class SettingsService implements ISettingsService {
 
   @Transactional()
   public async setGlobalDressOptions(globalDressOptionsDTO: GlobalDressOptionsDTO): Promise<GlobalDressOptionsDTO> {
-    const { categories: categoriesToUpdate } = globalDressOptionsDTO;
-    const { categories: currentCategories } = await this.getGlobalDressOptions();
+    const { categories: categoriesToUpdate, models: modelsToUpdate } = globalDressOptionsDTO;
+    const { categories: currentCategories, models: currentModels } = await this.getGlobalDressOptions();
     await this.updateCategories(currentCategories, categoriesToUpdate);
+    await this.updateModels(currentModels, modelsToUpdate);
     return this.getGlobalDressOptions();
   }
 
   private async updateCategories(currentCategories: CategoryDTO[], categoriesToSave: CategoryDTO[]): Promise<void> {
     const entities = this.categoryConverter.convertToEntity(categoriesToSave);
     const entitiesIds = entities.map((entity)=> entity.id ).filter(id => !!id);
+    const currentIds = currentCategories.map(category => category.id);
+    this.checkIds(currentIds, entitiesIds);
     const idsToDelete = currentCategories.filter((category)=> !entitiesIds.includes(category.id)).map(category => category.id);
     await this.categoryEntityRepository.save(entities);
     if (idsToDelete.length) {
       await this.categoryEntityRepository.delete(idsToDelete);
+    }
+  }
+
+  private async updateModels(currentModels: SimpleListSetting[], modelsToSave: SimpleListSetting[]): Promise<void>{
+    const entities = this.simpleListSettingConverter.convertToEntity(modelsToSave);
+    const entitiesIds = entities.map(entity => entity.id).filter(id => !!id);
+    const currentIds = currentModels.map(model => model.id);
+    this.checkIds(currentIds, entitiesIds);
+    const idsToDelete = currentModels.filter(model => !entitiesIds.includes(model.id)).map(model => model.id);
+    if (idsToDelete.length){
+      await this.modelEntityRepository.delete(idsToDelete);
+    }
+    await this.modelEntityRepository.save(entities);
+  }
+
+  private checkIds(currentIds: number[], newEntitiesIds: number[]): void{
+    if (currentIds.length && newEntitiesIds.length &&
+        newEntitiesIds.filter(id => currentIds.includes(id)).length !== newEntitiesIds.length){
+      throw new BadRequestException(INVALID_ID_TO_UPDATE);
     }
   }
 
