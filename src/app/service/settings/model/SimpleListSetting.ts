2a3663d72957d5e6cda136c392b614da2494987a
eviv2206 <zhenya.saprin@mail.ru>
2022-10-13 01:14:05 +0300

    feat(HAP-93): update message

--- src/app/service/settings/model/SimpleListSetting.ts
+++ src/app/service/settings/model/SimpleListSetting.ts
@@ -1,7 +1,7 @@
 import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
 import {
-  CATEGORY_TOO_LONG,
-  CATEGORY_TOO_SHORT,
+  DESCRIPTION_TOO_LONG,
+  DESCRIPTION_TOO_SHORT,
   INVALID_DESCRIPTION_DETECTED,
 } from '../../../messages/constants/messages.constants';
 
@@ -12,8 +12,8 @@ export class SimpleListSetting {
     id?: number;
 
     @IsString()
-    @MinLength(MIN_LENGTH_CATEGORY, { message: CATEGORY_TOO_SHORT })
-    @MaxLength(MAX_LENGTH_CATEGORY, { message: CATEGORY_TOO_LONG })
+    @MinLength(MIN_LENGTH_CATEGORY, { message: DESCRIPTION_TOO_SHORT })
+    @MaxLength(MAX_LENGTH_CATEGORY, { message: DESCRIPTION_TOO_LONG })
     @Matches(/^[а-яА-Яa-zA-Z0-9_.]+$/, { message: INVALID_DESCRIPTION_DETECTED })
     description: string;
 }
