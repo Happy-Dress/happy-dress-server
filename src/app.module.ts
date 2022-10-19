0878e12cc6dd54c28fbc9a0a21b7a0b829b7bba3
eviv2206 <zhenya.saprin@mail.ru>
2022-10-19 23:20:08 +0300

    feat(HAP-105): add health check

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ControllerModule } from './app/controller/controller.module';
 import { CustomConfigModule } from './config/custom-config.module';
+import { HealthModule } from './health.module';
+import { HttpModule } from '@nestjs/axios';
+import { TerminusModule } from '@nestjs/terminus';
 
 @Module({
   imports: [
     CustomConfigModule,
     ControllerModule,
+    HealthModule,
+    TerminusModule,
+    HttpModule,
   ],
 })
 export class AppModule {}
