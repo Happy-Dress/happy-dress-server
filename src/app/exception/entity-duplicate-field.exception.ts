427d6ee664797d740c02d3d0a10d0fa8ed152c55
eviv2206 <zhenya.saprin@mail.ru>
2023-02-15 20:35:28 +0300

    Revert "feat(HAP-195): update handling error not found by ids"
    This reverts commit 2af23f8f6b1006996c164128f2d18d58d2f21fc8.

--- src/app/exception/entity-duplicate-field.exception.ts
+++ src/app/exception/entity-duplicate-field.exception.ts
@@ -3,7 +3,7 @@ import { DUPLICATE_ENTRY_ENTITY } from '../messages/constants/messages.constants
 export class EntityDuplicateFieldException extends Error {
   constructor(entityName: string) {
       super(DUPLICATE_ENTRY_ENTITY
-        .replace('$ENTITY_NAME', entityName)
+        .replace('$ENTITY', entityName)
       );
   }
 }
