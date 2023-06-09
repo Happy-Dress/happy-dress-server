a96ff6005ddc12faf1ab5c524c930834d314d0d5
eviv2206 <zhenya.saprin@mail.ru>
2023-06-09 14:59:15 +0300

    feat(HAP-245): set min image size to 1kb

--- src/app/service/image/constants/validate-image/validate-image.constants.ts
+++ src/app/service/image/constants/validate-image/validate-image.constants.ts
@@ -1,5 +1,5 @@
 export const MAX_IMAGE_SIZE = 1e7;
-export const MIN_IMAGE_SIZE = 1e5;
+export const MIN_IMAGE_SIZE = 1e3;
 export const COEFFICIENT_BYTES_TO_MEGABYTES = 1e-6;
 export const IMAGE_EXTENSIONS = ['image/png', 'image/jpeg', 'image/jpg', 'image/raw'];
 export const MIN_AMOUNT = 0;
