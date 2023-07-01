013b7d4161fd757b0fd821ff040c1b67f47efb15
eviv2206 <zhenya.saprin@mail.ru>
2023-07-01 21:14:29 +0300

    feat(HAP-249): add blog converter test

--- src/test/test-utils/mock-dto-generators.ts
+++ src/test/test-utils/mock-dto-generators.ts
@@ -13,6 +13,7 @@ import { ProductColorSizeDto } from '../../app/service/products/model/product-co
 import { ProductColorImageDto } from '../../app/service/products/model/product-color-image.dto';
 import { ProductSearchDto } from '../../app/service/products/model/product-search.dto';
 import { ProductSearchViewDto } from '../../app/service/products/model/product-search.view.dto';
+import { BlogDto } from '../../app/service/blog/model/blog.dto';
 
 export function generateGlobalDressOptionsDto(): GlobalDressOptionsDto {
   return {
@@ -149,5 +150,15 @@ export function generateProductSearchViewDto(): ProductSearchViewDto {
     totalPages: 1,
     currentPage: productSearchDto.page,
     totalItems: 1,
+  };
+}
+
+export function generateBlogDto(): BlogDto {
+  return {
+    id: 1,
+    name: 'plain text',
+    shortDescription: 'plain text',
+    isPublished: true,
+    htmlLinkBlog: 'plain link',
   };
 }
